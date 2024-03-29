3c199e518bb1582f826ba35f31a0441c20d65aa8
yuhengr <e0957964@u.nus.edu>
2024-03-29 23:03:58 +0800

    Add skeleton for class CommandList

--- src/main/java/seedu/duke/CommandList.java
+++ src/main/java/seedu/duke/CommandList.java
@@ -1,4 +1,16 @@
 package seedu.duke;
 
 public class CommandList {
+
+    public static String TOPIC = "";
+
+    public static String BYE = "";
+
+    public static String SOLUTION = "";
+
+    public static String EXPLANATION = "";
+
+    public static String HELP = "";
+
+    public static String RESULTS = "";
 }
